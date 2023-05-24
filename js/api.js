0dd62649020221981193597d8324b2eca0d9db6e
Seman_Kim <sdoramkim@gmail.com>
2023-05-24 15:49:01 +0900

    :sparkles:Feat: 전체 적용js
    js/api.js : url 기본 경로, payload정보 추가

--- js/api.js
+++ js/api.js
@@ -1,1 +1,7 @@
 // 전체 적용 js
+const frontend_base_url = "http://127.0.0.1:5500"
+const backend_base_url = "http://127.0.0.1:8000"
+
+const payload = localStorage.getItem("payload");
+const payload_parse = JSON.parse(payload);
+const token = localStorage.getItem("access");
