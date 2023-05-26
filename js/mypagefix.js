ecb70561d57d29674aff1ab232d6832f645b698d
BenCyera <dltpgml1124@gmail.com>
2023-05-26 22:28:47 +0900

    :sparkles:Feat: 탈퇴 버튼 추가, Mypage로 이동 추가, 버튼 예외사항 추가, funtion 위치변경
    doc/mypagefix.html : 회원 탈퇴하기 버튼 추가
    js/loader.js : mypageLink를 현재 로그인 한 회원의 정보페이지로 변경
    js/mypagefix.js : 탈퇴 기능 추가, 탈퇴 시 localStorage에서 정보를 지워주고 로그아웃해줌
    js/mypage.js : 팔로우/내정보수정 버튼 user_id에 따라 노출 유무 변경, function 위치 변경, status 변경

--- js/mypagefix.js
+++ js/mypagefix.js
@@ -31,7 +31,31 @@ async function fixingUserInfo() {
 }) 
 if (response.status == 200) {
     alert("수정완료")
-    window.location.replace(`${frontend_base_url}/users/mypage/${user_id}/`);
+    window.location.href = `${frontend_base_url}/doc/mypage.html?user_id=${user_id}`
 } else {
     alert(response.status)
+}}
+
+
+async function withDrawUser() {
+    let user_id = payload_parse.user_id
+    const response = await fetch(`${backend_base_url}/users/mypage/${user_id}`,{
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        method: 'DELETE'
+    })
+    if (response.status == 200) {
+        localStorage.removeItem("access")
+        localStorage.removeItem("refresh")
+        localStorage.removeItem("payload")
+        response_json = await response.json()
+        alert(response_json.message)
+        window.location.href = `${frontend_base_url}`
+    } else if (response.status == 400) {
+        response_json = await response.json()
+        alert(response_json.message)
+        window.location.href = `${frontend_base_url}`
+    } else {
+        console.log(response.status)
 }}
