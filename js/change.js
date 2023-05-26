d6b258d432412db7b1149f79dfe12b2a27a74e15
Kim Kyungjin <grace8362@naver.com>
2023-05-26 19:47:00 +0900

    :sparkles:Feat: 이미지 파일 드래그 및 선택하기 추가 #8

---
+++ js/change.js
@@ -0,0 +1,68 @@
+const backEndBaseUrl = "http://127.0.0.1:8000"
+
+// 이미지 업로드
+const dropArea = document.querySelector(".drag-area"),
+    dragText = dropArea.querySelector("header"),
+    button = dropArea.querySelector("button"),
+    input = dropArea.querySelector("input");
+let file;
+
+/* ---------------------------------------------------------------------------- */
+
+
+input.addEventListener("change", function () {
+    file = this.files[0];
+    dropArea.classList.add("active");
+    showFile();
+})
+
+
+dropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Release to Upload File";
+})
+
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+})
+
+dropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    showFile();
+})
+
+
+function showFile() {
+    let fileType = file.type;
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+    if (validExtensions.includes(fileType)) {
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            let fileURL = fileReader.result;
+            let imgTag = `<img src="${fileURL}" alt="">`;
+            dropArea.innerHTML = imgTag;
+        }
+        fileReader.readAsDataURL(file);
+    } else {
+        alert("이미지 파일이 아닙니다.");
+        dropArea.classList.remove("active");
+        dragText.textContent = "드래그 하여 이미지 업로드";
+    }
+}
+
+/* ---------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
+
+
+
+
